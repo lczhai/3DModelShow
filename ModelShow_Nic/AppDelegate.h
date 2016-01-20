6ae91b75e1bc3cfb09b70c0280f3cbd06aa1c18d
shining3d <zzl11081@gmail.com>
2016-01-20 14:47:45 +0800

    Initial Commit

---
+++ ModelShow_Nic/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ModelShow_Nic
+//
+//  Created by shining3d on 16/1/20.
+//  Copyright © 2016年 shining3d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ModelShow_Nic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ModelShow_Nic
+//
+//  Created by shining3d on 16/1/20.
+//  Copyright © 2016年 shining3d. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
